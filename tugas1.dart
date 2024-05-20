f33d69027e274b9723b8fc0478b99ebef15ab013
renoalpandi <reno.mhs22@ti.poligon.ac.id>
2024-05-20 14:38:23 +0700

    first commit

---
+++ tugas1.dart
@@ -0,0 +1,477 @@
+// tugas minggu 1
+
+// buatlah repositori
+// tugas1dart di github dan didalamnya
+
+// ada file tugas1.dart
+
+// kirimkan link repositori
+// yang sudah selesai tugas berikut
+
+void main() {
+  print("exercise1 = ${exercise1()}");
+  print("exercise2 = ${exercise2()}");
+  print("exercise3 = ${exercise3()}");
+  print("exercise4 = ${exercise4()}");
+  print("exercise5 = ${exercise5()}");
+  print("exercise6 = ${exercise6()}");
+  print("exercise7 = ${exercise7()}");
+  print("exercise8 = ${exercise8()}");
+  print("exercise9 = ${exercise9()}");
+  print("exercise10 = ${exercise10()}");
+  print("exercise11 = ${exercise11()}");
+  print("exercise12() = ${exercise12()}");
+  print("exercise13() = ${exercise13()}");
+  print("exercise14() = ${exercise14()}");
+  print("exercise15() = ${exercise15()}");
+  print("exercise16() = ${exercise16()}");
+  print("exercise17() = ${exercise17()}");
+  print("exercise18() = ${exercise18()}");
+  print("exercise19() = ${exercise19()}");
+  print("exercise20() = ${exercise20()}");
+  print("exercise21() = ${exercise21()}");
+  print("exercise22() = ${exercise22()}");
+  print("exercise23() = ${exercise23()}");
+  print("exercise24() = ${exercise24()}");
+  print("exercise25() = ${exercise25()}");
+  print("exercise26() = ${exercise26()}");
+  print("exercise27() = ${exercise27()}");
+  print("exercise28() = ${exercise28()}");
+  print("exercise29() = ${exercise29()}");
+  print("exercise30() = ${exercise30()}");
+  print("exercise31() = ${exercise31()}");
+  print("exercise32() = ${exercise32()}");
+  print("exercise33() = ${exercise33()}");
+  print("exercise34() = ${exercise34()}");
+  print("exercise35() = ${exercise35()}");
+  print("exercise36() = ${exercise36()}");
+  print("exercise37() = ${exercise37()}");
+  print("exercise38() = ${exercise38()}");
+  print("exercise39() = ${exercise39()}");
+  print("exercise40() = ${exercise40()}");
+  print("exercise41() = ${exercise41()}");
+  print("exercise42() = ${exercise42()}");
+  print("exercise43() = ${exercise43()}");
+  print("exercise43() = ${exercise43()}");
+  print("exercise45() = ${exercise45()}");
+}
+
+// Exercise 1
+bool? exercise1() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data int dan beri nilai 10
+// TODO: Tulis jawabanmu di bawah ini
+  int output = 10;
+// --- End of Answer ---
+  return output is int && output == 10;
+}
+
+// Exercise 2
+bool? exercise2() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data int dan beri nilai 42
+// TODO: Tulis jawabanmu di bawah ini
+  int output = 42;
+// --- End of Answer ---
+  return output is int && output == 42;
+}
+
+// Exercise 3
+bool? exercise3() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data int dan beri nilai -7
+// TODO: Tulis jawabanmu di bawah ini
+  int output = -7;
+// --- End of Answer ---
+  return output is int && output == -7;
+}
+
+// Exercise 4
+bool? exercise4() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data int dan beri nilai 0
+// TODO: Tulis jawabanmu di bawah ini
+  int output = 0;
+// --- End of Answer ---
+  return output is int && output == 0;
+}
+
+// Exercise 5
+bool? exercise5() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data int dan beri nilai 99999
+// TODO: Tulis jawabanmu di bawah ini
+  int output = 99999;
+// --- End of Answer ---
+  return output is int && output == 99999;
+}
+
+// Exercise 6
+bool? exercise6() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data int dan beri nilai -12345
+// TODO: Tulis jawabanmu di bawah ini
+  int output = -12345;
+// --- End of Answer ---
+  return output is int && output == -12345;
+}
+
+// Exercise 7
+bool? exercise7() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data int dan beri nilai 1000000
+// TODO: Tulis jawabanmu di bawah ini
+  int output = 1000000;
+// --- End of Answer ---
+  return output is int && output == 1000000;
+}
+
+// Exercise 8
+bool? exercise8() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data int dan beri nilai -987654
+// TODO: Tulis jawabanmu di bawah ini
+  int output = -987654;
+// --- End of Answer ---
+  return output is int && output == -987654;
+}
+
+// Exercise 9
+bool? exercise9() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data int dan beri nilai 888
+// TODO: Tulis jawabanmu di bawah ini
+  int output = 888;
+// --- End of Answer ---
+  return output is int && output == 888;
+}
+
+// Exercise 10
+bool? exercise10() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data int dan beri nilai -555
+// TODO: Tulis jawabanmu di bawah ini
+  int output = -555;
+// --- End of Answer ---
+  return output is int && output == -555;
+}
+
+// Exercise 11
+bool? exercise11() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data double dan beri nilai 3.14
+// TODO: Tulis jawabanmu di bawah ini
+  double output = 3.14;
+// --- End of Answer ---
+  return output is double && output == 3.14;
+}
+
+// Exercise 12
+bool? exercise12() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data double dan beri nilai 2.71828
+// TODO: Tulis jawabanmu di bawah ini
+  double output = 2.71828;
+// --- End of Answer ---
+  return output is double && output == 2.71828;
+}
+
+// Exercise 13
+bool? exercise13() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data double dan beri nilai -5.67
+// TODO: Tulis jawabanmu di bawah ini
+  double output = -5.67;
+// --- End of Answer ---
+  return output is double && output == -5.67;
+}
+
+// Exercise 14
+bool? exercise14() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data double dan beri nilai 0.0
+// TODO: Tulis jawabanmu di bawah ini
+  double output = 0.0;
+// --- End of Answer ---
+  return output is double && output == 0.0;
+}
+
+// Exercise 15
+bool? exercise15() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data double dan beri nilai 12345.6789
+// TODO: Tulis jawabanmu di bawah ini
+  double output = 12345.6789;
+// --- End of Answer ---
+  return output is double && output == 12345.6789;
+}
+
+// Exercise 16
+bool? exercise16() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data double dan beri nilai 7.5
+// TODO: Tulis jawabanmu di bawah ini
+  double output = 7.5;
+// --- End of Answer ---
+  return output is double && output == 7.5;
+}
+
+// Exercise 17
+bool? exercise17() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data double dan beri nilai -2.25
+// TODO: Tulis jawabanmu di bawah ini
+  double output = -2.25;
+// --- End of Answer ---
+  return output is double && output == -2.25;
+}
+
+// Exercise 18
+bool? exercise18() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data double dan beri nilai 0.01
+// TODO: Tulis jawabanmu di bawah ini
+  double output = 0.01;
+// --- End of Answer ---
+  return output is double && output == 0.01;
+}
+
+// Exercise 19
+bool? exercise19() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data double dan beri nilai 123.456
+// TODO: Tulis jawabanmu di bawah ini
+  double output = 123.456;
+// --- End of Answer ---
+  return output is double && output == 123.456;
+}
+
+// Exercise 20
+bool? exercise20() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data double dan beri nilai -9876.54321
+// TODO: Tulis jawabanmu di bawah ini
+  double output = -9876.54321;
+// --- End of Answer ---
+  return output is double && output == -9876.54321;
+}
+
+// Exercise 21
+bool? exercise21() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data boolean dan beri nilai true
+// TODO: Tulis jawabanmu di bawah ini
+  bool output = true;
+// --- End of Answer ---
+  return output is bool && output == true;
+}
+
+// Exercise 22
+bool? exercise22() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data boolean dan beri nilai false
+// TODO: Tulis jawabanmu di bawah ini
+  bool output = false;
+// --- End of Answer ---
+  return output is bool && output == false;
+}
+
+// Exercise 23
+bool? exercise23() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data boolean dan beri nilai true
+// TODO: Tulis jawabanmu di bawah ini
+  bool output = true;
+// --- End of Answer ---
+  return output is bool && output == true;
+}
+
+// Exercise 24
+bool? exercise24() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data boolean dan beri nilai false
+// TODO: Tulis jawabanmu di bawah ini
+  bool output = false;
+// --- End of Answer ---
+  return output is bool && output == false;
+}
+
+// Exercise 25
+bool? exercise25() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data boolean dan beri nilai true
+// TODO: Tulis jawabanmu di bawah ini
+  bool output = true;
+// --- End of Answer ---
+  return output is bool && output == true;
+}
+
+// Exercise 26
+bool? exercise26() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data boolean dan beri nilai false
+// TODO: Tulis jawabanmu di bawah ini
+  bool output = false;
+// --- End of Answer ---
+  return output is bool && output == false;
+}
+
+// Exercise 27
+bool? exercise27() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data boolean dan beri nilai true
+// TODO: Tulis jawabanmu di bawah ini
+  bool output = true;
+// --- End of Answer ---
+  return output is bool && output == true;
+}
+
+// Exercise 28
+bool? exercise28() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data boolean dan beri nilai false
+// TODO: Tulis jawabanmu di bawah ini
+  bool output = false;
+// --- End of Answer ---
+  return output is bool && output == false;
+}
+
+// Exercise 29
+bool? exercise29() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data boolean dan beri nilai true
+// TODO: Tulis jawabanmu di bawah ini
+  bool output = true;
+// --- End of Answer ---
+  return output is bool && output == true;
+}
+
+// Exercise 30
+bool? exercise30() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data boolean dan beri nilai false
+// TODO: Tulis jawabanmu di bawah ini
+  bool output = false;
+// --- End of Answer ---
+  return output is bool && output == false;
+}
+
+// Exercise 31
+bool? exercise31() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data String dan beri nilai "Hello, World!"
+// TODO: Tulis jawabanmu di bawah ini
+  String output = "Hello, World!";
+// --- End of Answer ---
+  return output is String && output == "Hello, World!";
+}
+
+// Exercise 32
+bool? exercise32() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data String dan beri nilai "Dart is fun!"
+// TODO: Tulis jawabanmu di bawah ini
+  String output = "Dart is fun!";
+// --- End of Answer ---
+  return output is String && output == "Dart is fun!";
+}
+
+// Exercise 33
+bool? exercise33() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data String dan beri nilai "12345"
+// TODO: Tulis jawabanmu di bawah ini
+  String output = "12345";
+// --- End of Answer ---
+  return output is String && output == "12345";
+}
+
+// Exercise 34
+bool? exercise34() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data String dan beri nilai "3.14159"
+// TODO: Tulis jawabanmu di bawah ini
+  String output = "3.14159";
+// --- End of Answer ---
+  return output is String && output == "3.14159";
+}
+
+// Exercise 35
+bool? exercise35() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data String dan beri nilai "true"
+// TODO: Tulis jawabanmu di bawah ini
+  String output = "true";
+// --- End of Answer ---
+  return output is String && output == "true";
+}
+
+// Exercise 36
+bool? exercise36() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data String dan beri nilai "false"
+// TODO: Tulis jawabanmu di bawah ini
+  String output = "false";
+// --- End of Answer ---
+  return output is String && output == "false";
+}
+
+// Exercise 37
+bool? exercise37() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data String dan beri nilai "Hello, Dart!"
+// TODO: Tulis jawabanmu di bawah ini
+  String output = "Hello, Dart!";
+// --- End of Answer ---
+  return output is String && output == "Hello, Dart!";
+}
+
+// Exercise 38
+bool? exercise38() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data String dan beri nilai "42"
+// TODO: Tulis jawabanmu di bawah ini
+  String output = "42";
+// --- End of Answer ---
+  return output is String && output == "42";
+}
+
+// Exercise 39
+bool? exercise39() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data String dan beri nilai "3.14"
+// TODO: Tulis jawabanmu di bawah ini
+  String output = "3.14";
+// --- End of Answer ---
+  return output is String && output == "3.14";
+}
+
+// Exercise 40
+bool? exercise40() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data String dan beri nilai "Hello, World!"
+// TODO: Tulis jawabanmu di bawah ini
+  String output = "Hello, World!";
+// --- End of Answer ---
+  return output is String && output == "Hello, World!";
+}
+
+// Exercise 41
+bool? exercise41() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data DateTime dan beri nilai tanggal 1 Januari 2022
+// TODO: Tulis jawabanmu di bawah ini
+  DateTime output = DateTime(2022, 1, 1);
+// --- End of Answer ---
+  return output is DateTime &&
+      output.year == 2022 &&
+      output.month == 1 &&
+      output.day == 1;
+}
+
+// Exercise 42
+bool? exercise42() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data DateTime dan beri nilai tanggal 31 Desember 2000
+// TODO: Tulis jawabanmu di bawah ini
+  DateTime output = DateTime(2000, 12, 31);
+// --- End of Answer ---
+  return output is DateTime &&
+      output.year == 2000 &&
+      output.month == 12 &&
+      output.day == 31;
+}
+
+// Exercise 43
+bool? exercise43() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data DateTime dan beri nilai tanggal 15 September 1995
+// TODO: Tulis jawabanmu di bawah ini
+  DateTime output = DateTime(1995, 9, 15);
+// --- End of Answer ---
+  return output is DateTime &&
+      output.year == 1995 &&
+      output.month == 9 &&
+      output.day == 15;
+}
+
+// Exercise 44
+bool? exercise44() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data DateTime dan beri nilai tanggal 10 Mei 2023
+// TODO: Tulis jawabanmu di bawah ini
+  DateTime output = DateTime(2023, 5, 10);
+// --- End of Answer ---
+  return output is DateTime &&
+      output.year == 2023 &&
+      output.month == 5 &&
+      output.day == 10;
+}
+
+// Exercise 45
+bool? exercise45() {
+// ? Instruksi: Deklarasikan sebuah variabel "output" dengan tipe data DateTime dan beri nilai tanggal 24 November 2010
+// TODO: Tulis jawabanmu di bawah ini
+  DateTime output = DateTime(2010, 11, 24);
+// --- End of Answer ---
+  return output is DateTime &&
+      output.year == 2010 &&
+      output.month == 11 &&
+      output.day == 24;
+}
